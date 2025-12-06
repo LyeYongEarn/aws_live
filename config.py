c0b78abf2c5d1724a3accb3a9cbac81720786d03
LyeYongEarn <yongearnl@gmail.com>
2025-12-06 12:42:47 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cshyj4b7fyjf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee_db"
+custombucket = "lyeyongearn-bucket"
+customregion = "us-east-1"
+
